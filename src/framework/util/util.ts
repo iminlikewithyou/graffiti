5e04e7a183d814880c2490b1f0f7d73f0e773dcf
66minmin <christian@omg.games>
2023-10-03 06:29:01 -0500

    start utility

---
+++ src/framework/util/util.ts
@@ -0,0 +1,27 @@
+import { ServerStorage } from "@rbxts/services"
+
+let graffitiInstance: Instance | undefined = undefined;
+
+function thereIsNoGraffitiInstance() {
+  warn("There is no active Graffiti instance. Creating one now.");
+
+  graffitiInstance = new Instance("Atmosphere");
+  graffitiInstance.Name = "Graffiti Editor";
+  graffitiInstance.Parent = ServerStorage;
+
+  return graffitiInstance;
+}
+
+export function getGraffitiContainer()
+
+export function getGraffitiLocation() {
+  return ServerStorage.FindFirstChild("Graffiti Editor")
+}
+
+export function getGraffitiInstance() {
+  return graffitiInstance;
+}
+
+export function getBlueprintLocation(identifier: string) {
+  return 
+}
