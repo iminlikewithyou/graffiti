e854cfbade5ef95769045808cc134b48d784c367
66minmin <christian@omg.games>
2023-10-06 06:25:24 -0500

    update Blueprint to use BlueprintReferences

--- src/framework/models/Blueprint.ts
+++ src/framework/models/Blueprint.ts
@@ -1,12 +1,20 @@
+import BlueprintReference from "./BlueprintReference";
+
+export type BlueprintCreateable = BlueprintReference;
+
 export default class Blueprint {
-  public model: Model;
+  public blueprintReference: BlueprintReference;
   // public scale: number;
 
   // TODO: In the future, it should be an ID which retrieves a model
   // from the model registry. For now, it's simpler to just pass the
   // model directly.
-  constructor(model: Model) {
-    this.model = model;
+  constructor(createable: BlueprintCreateable) {
+    if (createable instanceof BlueprintReference) {
+      this.blueprintReference = createable;
+    } else {
+      error("Blueprint couldn't be created from", createable);
+    }
   }
 
   scale(scale: number) {
---
+++ src/framework/models/BlueprintReference.ts
@@ -0,0 +1,19 @@
+export default class BlueprintReference {
+  public blueprintInstance: Instance;
+  public changeReferenceEvent: BindableEvent = new Instance("BindableEvent");
+
+  constructor(blueprintInstance: Instance) {
+    this.blueprintInstance = blueprintInstance;
+  }
+
+  changeReference(blueprintInstance: Instance) {
+    const oldBlueprintInstance = this.blueprintInstance;
+    this.blueprintInstance = blueprintInstance;
+
+    this.changeReferenceEvent.Fire(blueprintInstance, oldBlueprintInstance);
+  }
+
+  destroy() {
+    this.changeReferenceEvent.Destroy();
+  }
+}
