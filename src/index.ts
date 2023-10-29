64d789a4e3f60f1909c86b39840e3f371b35e9ea
66minmin <christian@omg.games>
2023-10-29 17:55:45 -0500

    do exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 export function makeHello(name: string) {
 	return `Hello from ${name}!`;
 }
+
+// Framework
+// Containers
+// Editor
+// Layers
+export * from './framework/layers/ViewportLayer';
+// Models
+export * from './framework/models/Blueprint';
+export * from './framework/models/BlueprintReference';
+// Scenes
+// Server
+// Util
+
+export * from './utils/Cameraman';
