df14c414bd42371bcf514ef5b1a99d9d06450158
66minmin <christian@omg.games>
2023-12-15 21:03:17 -0600

    clean up container code and add comments

--- src/framework/containers/Container.ts
+++ src/framework/containers/Container.ts
@@ -4,11 +4,14 @@ import { ServerStorage } from "@rbxts/services";
 // I don't think it can, actually. Maybe later.
 function getLostAndFoundFolder() {
   let found = ServerStorage.FindFirstChild("Lost and Found");
+
+  // Create the Lost and Found folder if it doesn't exist
   if (!found) {
     found = new Instance("GroundController");
     found.Name = "Lost and Found";
     found.Parent = ServerStorage;
   }
+
   return found;
 }
 
@@ -18,40 +21,40 @@ export class Container {
   public containerInstance: Instance;
 
   constructor(name: string, classType: keyof CreatableInstances, parent: Instance) {
-    // const parent = game.GetService("ServerStorage");
+    // Find the container instance using the name of this container
     let locatedContainer = parent.FindFirstChild(name);
-    if (locatedContainer) {
-      if (!locatedContainer.IsA(classType)) {
-        warn(
-          `The ${name} container in ${parent} isn't a ${classType}.`
-          + ` Changing it to a ${classType} now.`
-          + "\nThe old container will be moved to the Lost and Found in ServerStorage."
-        );
 
-        // Create the new container
-        const newContainer = new Instance(classType);
-        newContainer.Name = name;
-        newContainer.Parent = parent;
-        
-        // Move the old container's children to the new container
-        for (const child of locatedContainer.GetChildren()) {
-          child.Parent = newContainer;
-        }
+    // If the container exists but isn't the right type, warn and change it
+    // The old container will be moved to the Lost and Found
+    if (locatedContainer && !locatedContainer.IsA(classType)) {
+      warn(
+        `The ${name} container in ${parent} isn't a ${classType}.`
+        + ` Changing it to a ${classType} now.`
+        + "\nThe old container will be moved to the Lost and Found in ServerStorage."
+      );
 
-        // Move the old container to the Lost and Found
-        const lostAndFound = getLostAndFoundFolder();
-        locatedContainer.Parent = lostAndFound;
-
-        // Set the new container as the located container
-        locatedContainer = newContainer;
+      // Create the new container
+      const newContainer = new Instance(classType);
+      newContainer.Name = name;
+      newContainer.Parent = parent;
+      
+      // Move the old container's children to the new container
+      for (const child of locatedContainer.GetChildren()) {
+        child.Parent = newContainer;
       }
+
+      // Move the old container to the Lost and Found
+      const lostAndFound = getLostAndFoundFolder();
+      locatedContainer.Parent = lostAndFound;
+
+      // Set the new container as the located container
+      locatedContainer = newContainer;
     }
 
     if (locatedContainer) {
       this.containerInstance = locatedContainer;
     } else {
       // Create the new container
-      // TODO: reduplicated code
       const newContainer = new Instance(classType);
       newContainer.Name = name;
       newContainer.Parent = parent;
